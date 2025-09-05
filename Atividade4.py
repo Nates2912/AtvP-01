28efda881e53f200a1835bb15b4bb0c9f5e32580
Nates2912 <srakawaiiuwu@gmail.com>
2025-09-05 16:57:35 -0300

    Add files via upload

---
+++ Atividade4.py
@@ -0,0 +1,18 @@
+#Cleaning the terminal
+import os
+os.system("cls")
+
+#Big test 4!!!
+apple = int(input("How many apples are in your cart: "))
+straw = int(input("How many strawberries are in your cart: "))
+ 
+
+
+if apple >= 12: 
+    apple * 1.50
+    print(f"You bought {apple} apples.")
+    
+else: 
+    apple * 1.80
+    print(f"You bought {apple} .")
+    
