aaba7fb60c0b79b464ec4dd953351a27cbc91d75
Nates2912 <srakawaiiuwu@gmail.com>
2025-09-05 15:33:15 -0300

    Add files via upload

---
+++ Atividade3.py
@@ -0,0 +1,19 @@
+#Cleaning the terminal
+import os
+os.system("cls")
+
+#Big test 3!!!
+
+A = int(input("Type the first number: "))
+B = int(input("Type the second number: "))
+
+C = A = B
+
+if A == B:
+    print(f"The result is {A + B}")
+
+else:
+    print(f"The result is {A * B}")
+
+
+
